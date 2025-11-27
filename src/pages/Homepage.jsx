4e9a0bd76fe70add052598528a99e59e5529b397
Davit Kutsia <132742480+DavitKutsia@users.noreply.github.com>
2025-11-28 01:10:50 +0400

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -24,7 +24,7 @@ export default function Homepage() {
       setError("");
       try {
         const res = await fetch(
-          `http://localhost:4000/products/`,
+          `https://forgotten-books-project-backend.vercel.app/products/`,
           {
             method: "GET",
             headers: {
