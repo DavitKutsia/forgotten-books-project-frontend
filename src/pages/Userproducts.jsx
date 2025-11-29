302d544d838d6b2e81804d003cfa0214479f34a0
Davit Kutsia <132742480+DavitKutsia@users.noreply.github.com>
2025-11-29 15:51:01 +0400

    Update Userproducts.jsx

--- src/pages/Userproducts.jsx
+++ src/pages/Userproducts.jsx
@@ -33,7 +33,7 @@ export default function UserProducts({variable}) {
 
       try {
         const res = await fetch(
-          "http://localhost:4000/auth/profile",
+          "https://forgotten-books-project-backend.vercel.app/auth/profile",
           {
             headers: { Authorization: `Bearer ${token}` },
           }
